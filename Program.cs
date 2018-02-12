7711ae701f1f6a801ae5becf9ac9b2f5af09e709
Imran Aliyev <imran.aliyev@gmail.com>
2018-02-12 22:53:01 +0100

    ReverseInteger method has been added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace Problem_Solving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReverseInteger(551);
+        }
+
+        public static void ReverseInteger(int input)
+        {
+            //Given a 32-bit signed integer, reverse digits of an integer.
+
+            int num = input;
+            string result = string.Empty;
+
+            while (num > 0)
+            {
+                result += (num % 10);
+                num = num / 10;
+            }
+
+            System.Console.WriteLine(input < 0 ? ("-" + result) : result);
         }
     }
+
 }
